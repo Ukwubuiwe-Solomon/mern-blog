19e50176709048d3cc036179898a5e6757beb856
Ukwubuiwe-Solomon <solomonchinedu@gmail.com>
2026-09-19 02:18:46 +0100

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("MongoDB is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-  console.log("Server is running");
+  console.log("Server is running on port 3000");
 });
